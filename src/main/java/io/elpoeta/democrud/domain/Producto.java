4b131f6d5c1ed55a2f33d0d3d8c4b74efd18fb53
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2018-05-27 00:04:33 -0300

    Removido cascade de @ManyToOne de entity producto

--- src/main/java/io/elpoeta/democrud/domain/Producto.java
+++ src/main/java/io/elpoeta/democrud/domain/Producto.java
@@ -2,7 +2,6 @@
 package io.elpoeta.democrud.domain;
 
 import java.math.BigDecimal;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
